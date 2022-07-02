72c3c853df9a0e755ad6b078dcef2624dae72f55
Bright Chigozie <goziedbrightest@gmail.com>
2022-07-02 16:00:46 -0700

    brightestworldz

--- I4G016932BVT/links/views.py
+++ I4G016932BVT/links/views.py
@@ -1,3 +1,35 @@
+from msilib.schema import ListView
+from pyexpat import model
+from re import L
+from typing import List
 from django.shortcuts import render
 
 # Create your views here.
+class PostListView(ListView):
+    model = Post
+
+    class  PostCreateView(CreateView):
+        model = Post
+
+fields = “__all__”
+
+success_url  = reverse_lazy(“links:all”)
+
+
+        class PostDetailView(DetailView):
+            model = Post
+
+
+        class PostUpdateView(UpdateView):
+            model = Post
+
+fields = “__all__”
+
+success_url  = reverse_lazy(“links:all”)
+
+        class PostDeleteView(DeletView):
+            model = Post
+
+fields = “__all__”
+
+success_url  = reverse_lazy(“links:all”)
